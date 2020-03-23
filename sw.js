f3e854598fe80091f57b52fcd8a799eb57a2fbc4
Christopher Wang <christopher.wang@zoom.us>
2020-03-23 10:18:31 +0800

    typo

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@ self.addEventListener('install', function(e) {
 
 self.addEventListener('fetch', function(e) {
     console.log(e.request.url);
-    e.repondWith(
+    e.respondWith(
         caches.match(e.request)
             .then( function(response) {
                 return response || fetch(e.request);
